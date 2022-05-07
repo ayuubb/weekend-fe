b875a091deec362a71392c35c58de8c7a1a6d7ca
ayuubb <ayubbkj2@gmail.com>
2022-05-07 22:46:14 +0800

    about section

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <section className="footer">
+    <section className="footer d-flex align-items-center">
       <div className="container d-flex justify-content-between align-items-center">
-        <p>
-          wknd@<span>2020</span>
+        <p className="footer-year">
+          wknd@<span className="year">2020</span>
         </p>
-        <div className="version p-1">
-          <p>alpha version 0.1</p>
-        </div>
+        <p className="version">alpha version 0.1</p>
       </div>
     </section>
   );
